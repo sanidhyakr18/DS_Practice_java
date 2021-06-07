8a274f420a45218fe61b79b33b41bcfc4c3d5e08
sanidhyakr18 <sanidhyakr18@gmail.com>
2021-06-07 18:01:06 +0530

    min cost climbing stairs

---
+++ src/LeetCode/MinCostClimbingStairs.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class MinCostClimbingStairs {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+        System.out.println(minCostClimbingStairs(arr));
+    }
+
+    public static int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[dp.length - 1];
+    }
+}
