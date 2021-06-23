b9f164f8cc833733f07f04378edcdb5bc5885a0e
sanidhyakr18 <sanidhyakr18@gmail.com>
2021-06-24 01:12:24 +0530

    arithmetic progression from sequence o(n) solution

--- src/LeetCode/CanMakeArithmeticProgressionFromSequence.java
+++ src/LeetCode/CanMakeArithmeticProgressionFromSequence.java
@@ -1,19 +1,41 @@
 package LeetCode;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 
 public class CanMakeArithmeticProgressionFromSequence {
     public static void main(String[] args) {
-        int[] arr = new int[]{5, 1, 2};
+        int[] arr = new int[]{5, 1, 3};
         System.out.println(canMakeArithmeticProgression(arr));
     }
 
+//    O(NlogN) Solution
+//    public static boolean canMakeArithmeticProgression(int[] arr) {
+//        Arrays.sort(arr);
+//        int diff = arr[1] - arr[0];
+//        for (int i = 2; i < arr.length; i++) {
+//            if (arr[i] - arr[i - 1] != diff)
+//                return false;
+//        }
+//        return true;
+//    }
+
+//    O(N) Solution
     public static boolean canMakeArithmeticProgression(int[] arr) {
-        Arrays.sort(arr);
-        int diff = arr[1] - arr[0];
-        for (int i = 2; i < arr.length; i++) {
-            if (arr[i] - arr[i - 1] != diff)
+        HashSet<Integer> set = new HashSet<>();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int j : arr) {
+            set.add(j);
+            max = Math.max(max, j);
+            min = Math.min(min, j);
+        }
+        int diff = (max - min) / (arr.length - 1);
+        for (int i = 1; i < arr.length; i++) {
+            if (!set.contains(min + i * diff)) {
                 return false;
+            }
         }
         return true;
     }
